234aedf893bbeeb7bc467eacc19d87a5e3b1aabf
fengran <1317813165@qq.com>
2015-11-14 17:44:00 +0800

    dafadvcv

--- mysite1/mysite1/settings.py
+++ mysite1/mysite1/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os111
+import os11111
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
--- mysite1/mysite1/wsgi.py
+++ mysite1/mysite1/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os111
+import os11111
 
 from django.core.wsgi import get_wsgi_application
 
