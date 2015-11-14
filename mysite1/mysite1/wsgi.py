631656aca83efda9cf73e4db6c6166f1fa98502c
fengran <1317813165@qq.com>
2015-11-14 17:48:52 +0800

    dsaddafadv

--- mysite1/mysite1/wsgi.py
+++ mysite1/mysite1/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os111
+import os
 
 from django.core.wsgi import get_wsgi_application
 
